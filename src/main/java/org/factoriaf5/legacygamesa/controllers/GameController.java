3d6189054d7c7c2be2205b9ea599ef3f0d72de35
Taiza18 <taizabrito@hotmail.com>
2021-06-14 15:37:33 +0200

    cambios home controller

--- src/main/java/org/factoriaf5/legacygamesa/controllers/GameController.java
+++ src/main/java/org/factoriaf5/legacygamesa/controllers/GameController.java
@@ -1,23 +1,15 @@
 package org.factoriaf5.legacygamesa.controllers;
-
 import org.factoriaf5.legacygamesa.services.GameService;
 import org.factoriaf5.legacygamesa.models.Game;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 @Controller
 public class GameController {
-
     private GameService gameService;
-
     @Autowired
     public GameController(GameService gameService) {
         this.gameService = gameService;
     }
-
     @GetMapping("/games")
     String listGames(Model model) {
         List<Game> games = gameService.allGame();

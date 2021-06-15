f3c6174ced3c713bffe61c8ac1747e537ddc7d53
Taiza18 <taizabrito@hotmail.com>
2021-06-15 10:48:24 +0200

    cambiando version java

--- src/main/java/org/factoriaf5/legacygamesa/controllers/GameController.java
+++ src/main/java/org/factoriaf5/legacygamesa/controllers/GameController.java
@@ -1,6 +1,10 @@
 package org.factoriaf5.legacygamesa.controllers;
 import org.factoriaf5.legacygamesa.services.GameService;
 import org.factoriaf5.legacygamesa.models.Game;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 @Controller

84d20a24b7fca5a36dee7a4af8cc28c56c142c13
Taiza18 <taizabrito@hotmail.com>
2021-06-17 12:26:11 +0200

    añadimos rutas en get mapping y creado archivo new y modificado barra de navegación

--- src/main/java/org/factoriaf5/legacygamesa/controllers/GameController.java
+++ src/main/java/org/factoriaf5/legacygamesa/controllers/GameController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 @Controller
@@ -21,4 +24,30 @@ public class GameController {
         model.addAttribute("games", games);
         return "games/all";
     }
+
+    @GetMapping("/new")
+    String getForm(Model model){
+        Game game = new Game();
+        model.addAttribute("game", game);
+        return "games/new";
+    }
+    @PostMapping("/new")
+    String addGame(@ModelAttribute Game game) {
+        gameService.save(game);
+        return "redirect:/games";
+    }
+    @GetMapping("edit/{id}")
+    String editGame(Model model, @PathVariable Long id){
+        Game game = gameService.findById(id);
+        model.addAttribute("game", game);
+        model.addAttribute("title", "Edit games");
+        return "games/new";
+    }
+
+    @GetMapping("/delete/{id}")
+    String removeGame(@PathVariable Long id){
+        gameService.delete(id);
+        return "redirect:/games";
+    }
+
 }

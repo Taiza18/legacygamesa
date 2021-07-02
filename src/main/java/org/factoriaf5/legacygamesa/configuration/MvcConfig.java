49599e1450b8eeaeb5f75f75c2cf86cb3207e064
Taiza18 <taizabrito@hotmail.com>
2021-07-02 09:21:52 +0200

    finalizamos funcion search

--- src/main/java/org/factoriaf5/legacygamesa/configuration/MvcConfig.java
+++ src/main/java/org/factoriaf5/legacygamesa/configuration/MvcConfig.java
@@ -1,4 +1,4 @@
-package org.factoriaf5.legacygamesa.controllers;
+package org.factoriaf5.legacygamesa.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
